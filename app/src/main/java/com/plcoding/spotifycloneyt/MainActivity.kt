492fcadcd970205c2d5f306b1ae8d41b89d41e9c
jenishdesai <jenishdesai63@gmail.com>
2023-04-01 15:25:53 +0530

    till four done

--- app/src/main/java/com/plcoding/spotifycloneyt/MainActivity.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/MainActivity.kt
@@ -2,8 +2,13 @@ package com.plcoding.spotifycloneyt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.RequestManager
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var glide : RequestManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
